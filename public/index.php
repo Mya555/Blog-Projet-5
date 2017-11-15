1979116c19cd0d2aa651900339154521f5a39824
Bella <text_me2@protonmail.com>
2017-11-15 16:19:12 +0100

    Création du dossier public

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+    define('ROOT', dirname(__DIR__));
+
+   require ROOT . '/app/App.php';
+
+
+
+
+    /*
+     * La fonction load s'occupe du chargement des class d'autoloading
+    */
+   App::load();
+
+
+    if (isset($_GET['p'])){
+
+        $page = $_GET['p'];
+    }
+    else{
+
+        $page = 'posts.index';
+
+
+    }
+
+
+        $page = explode('.', $page);
+
+
+        $controller = '\App\Controller\\'  . ucfirst($page[0]) . 'Controller';
+
+        $action = $page[1];
+
+        $controller = new $controller();
+
+        $controller->$action();
