ca6f5c56c9b9236f8189600b05064b55568eab57
Bella <text_me2@protonmail.com>
2017-11-15 16:32:13 +0100

    Création du dossier table dans app

---
+++ app/Table/PostTable.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Table;
+
+use Core\Table\Table;
+
+class PostTable extends Table
+{
+
+    protected $table = 'articles';
+
+
+
+    public function last()
+    {
+        return $this->query("
+
+        SELECT id, titre, contenu, DATE_FORMAT(dateDernierModif, 'le %d/%m/%Y à %Hh%imin%ss') AS dateDernierModif, auteur, chapo
+        
+        FROM articles
+        
+        ORDER BY dateDernierModif ASC
+        ");
+    }
+
+
+
+
+
+
+    public function find($id){
+
+        return $this->query("
+
+        SELECT id, titre, contenu, DATE_FORMAT(dateDernierModif, 'le %d/%m/%Y à %Hh%imin%ss') AS dateDernierModif, auteur, chapo
+        
+        FROM articles
+        
+         WHERE id = ? 
+        ", [$id], true);
+
+    }
+
+
+
+
+}
